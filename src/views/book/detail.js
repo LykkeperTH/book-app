189d60b7e737eb8c65ad31610a9fb514fbc8474c
Kittinan <rekise1@hotmail.com>
2020-11-10 22:30:23 +0700

    detailupdate

--- src/views/book/detail.js
+++ src/views/book/detail.js
@@ -1,7 +1,20 @@
 const DetailBookById = ({ book }) => {
   return (
-    <div className="box-detail">
-      <h2>{book.title}</h2>
+    <div>
+      <div className="box-detail">
+        <h2>{book.title}</h2>
+      </div>
+      <div className="justify-center d-flex mx-auto book-detail">
+        <div className="img-cover">
+          <img className="img-detail" src={book.imageUrl} alt="Avatar" />
+        </div>
+        <div className="detail-cover">
+          <div className="book-content">
+            <h3 className="mt-0p mb-0p">รายละเอียดและประเภท</h3>
+            <span>{book.description}</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
